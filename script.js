cc8b9ba64f8666bc5d9b88f2513f182182981db2
jarvisbyak-pack <jarvisbyak@gmail.com>
2026-09-11 11:11:07 +0530

    Initialize core JS interactions for AK PACK Fitness

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ console.log("AK PACK Fitness website initialized.");
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function(e) {
         e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
