e77e3e3418c1b1fdbc307c10851233123957795d
jarvisbyak-pack <jarvisbyak@gmail.com>
2026-09-12 16:24:15 +0530

    Initialize minimal directory structure for Batturaan Galaxy Viewer

---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <div className="text-white">Batturaan Galaxy Viewer - Loading...</div>
+  </React.StrictMode>,
+)
